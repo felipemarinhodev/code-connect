8ccc7b8b11a145527cc9685d23411792d4fa5f8b
Felipe Marinho <github@felipemarinho.dev>
2024-03-02 10:51:29 -0800

    a2.4 - Importing fonts of google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
+import { Prompt } from "next/font/google";
 
-import './globals.css';
+import "./globals.css";
 import { Aside } from "@/components/Aside";
 
 export const metadata: Metadata = {
@@ -8,13 +9,19 @@ export const metadata: Metadata = {
   description: "Uma rede social para devs",
 };
 
+const prompt = Prompt({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  display: "swap"
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={prompt.className}>
       <body>
         <div className="app-container">
         <Aside />
